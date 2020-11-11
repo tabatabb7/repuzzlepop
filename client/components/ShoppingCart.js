10302b57ebb067f01f6d03909cdbc7b006d02e83
tabatabb7 <yoosallyh@gmail.com>
2020-11-11 14:41:04 -0500

    fixed prettier issues in Shopping Cart component

--- client/components/ShoppingCart.js
+++ client/components/ShoppingCart.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import Navbar from './index'
-import Footer from './index'
+import {Navbar, Footer} from './index'
 
 export class ShoppingCart extends React.Component {
   render() {
@@ -9,23 +8,23 @@ export class ShoppingCart extends React.Component {
         <Navbar />
         <h2>List of Items:</h2>
         <div id="mappedproducts">
-          {/* {products.map((product) => {
-            ;<div>
+          {products.map(product => (
+            <div key="cart">
               <table>
                 <tr>
-                  <th>product.name</th>
-                  <th>product.resellPrice</th>
+                  <th>{product.name}</th>
+                  <th>{product.resellPrice}</th>
                 </tr>
               </table>
               <form>
                 <label htmlFor="quantity">Quantity: </label>
                 <input type="number" id="quantity" name="quantity" />
-                <button>Remove Item</button>
+                <button type="submit">Remove Item</button>
               </form>
             </div>
-          })} */}
+          ))}
         </div>
-        <button>Go To Checkout</button>
+        <button type="submit">Go To Checkout</button>
         <Footer />
       </div>
     )
