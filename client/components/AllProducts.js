0a82cf800e45fc0f3db128a1be2ad483e62c21d4
tabatabb7 <yoosallyh@gmail.com>
2020-11-11 17:21:30 -0500

    created productdetail component

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,44 +1,19 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import ProductDetail from './ProductDetail'
 
-export class AllProducts extends React.Component {
+export default class AllProducts extends React.Component {
   render() {
     return (
       <div>
-        <div>
-          <Navbar />
-        </div>
         <h2>All Products</h2>
         <div className="products">
           {products.length < 1
             ? 'No Products'
-            : products.map((product) => (
-                <div key={product.id} className="product-child">
-                  <NavLink to={`/products/${product.id}`}>
-                    <div key={product.id}>
-                      <h4>{product.name}</h4>
-                      <div className="campus-img">
-                        <img src={product.image} />
-                      </div>
-                      <h5>{product.origPrice}</h5>
-                      <h5>{product.resellPrice}</h5>
-                      <p>{product.description}</p>
-                      <h5>{product.rating}</h5>
-                    </div>
-                  </NavLink>
-                  <button
-                    className="add-to-cart-button"
-                    type="button"
-                    //   onClick={() => this.handleDelete(student.id)}
-                  >
-                    Add to Cart
-                  </button>
-                </div>
+            : products.map(product => (
+                <ProductDetail key={product.id} product={product} />
               ))}
-        </div>
-        <div>
-          <Footer />
         </div>
       </div>
     )
---
+++ client/components/ProductDetail.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export const ProductDetail = product => {
+  return (
+    <div className="product-detail-wrapper">
+      <div key={product.id} className="product-child">
+        <NavLink to={`/products/${product.id}`}>
+          <div key={product.id}>
+            <div className="product-name">{product.name}</div>
+            <div className="product-img">
+              <img src={product.image} />
+            </div>
+            <div className="product-orig-price">{product.origPrice}</div>
+            <div className="product-resell-price">{product.resellPrice}</div>
+            <div className="product-description">{product.description}</div>
+            <div className="product-rating">{product.rating}</div>
+          </div>
+        </NavLink>
+        <button
+          className="add-to-cart-button"
+          type="button"
+          //   onClick={() => this.handleDelete(student.id)}
+        >
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  )
+}
