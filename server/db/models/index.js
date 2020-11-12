2ec9a46d96d788880ef8c55a779b6a0ce3e1c75f
Ariel <arielespo1123@gmail.com>
2020-11-12 15:12:18 -0500

    WIP: refactoring database

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,53 +1,14 @@
 const User = require('./user')
 const Product = require('./product')
+const Order = require('./order')
+const CartItem = require('./cart_item')
 const db = require('../db')
 
-User.hasMany(Product, {
-  foreignKey: 'purchasedBy'
-})
-Product.belongsTo(User, {
-  foreignKey: 'purchasedBy'
-})
+User.hasMany(Order)
+Order.belongsTo(User)
 
-User.hasMany(Product, {
-  foreignKey: 'listedBy'
-})
-Product.belongsTo(User, {
-  foreignKey: 'listedBy'
-})
-
-// User.belongsToMany(Product, {
-//   foreignKey: 'purchasedBy',
-// })
-// User.belongsToMany(Product, {
-//   foreignKey: 'listedBy',
-// })
-// Product.belongsToMany(User, {
-//   foreignKey: 'listedBy'
-// })
-
-// const Movie = sequelize.define('Movie', { name: DataTypes.STRING });
-// const Actor = sequelize.define('Actor', { name: DataTypes.STRING });
-// const ActorMovies = sequelize.define('ActorMovies', {
-//   MovieId: {
-//     type: DataTypes.INTEGER,
-//     references: {
-//       model: Movie, // 'Movies' would also work
-//       key: 'id'
-//     }
-//   },
-//   ActorId: {
-//     type: DataTypes.INTEGER,
-//     references: {
-//       model: Actor, // 'Actors' would also work
-//       key: 'id'
-//     }
-//   }
-// });
-// Movie.belongsToMany(Actor, { through: ActorMovies });
-// Actor.belongsToMany(Movie, { through: ActorMovies });
-// Movie.belongsToMany(Actor, { through: ActorMovies });
-// Actor.belongsToMany(Movie, { through: ActorMovies });
+Product.belongsToMany(Order, {through: CartItem})
+Order.belongsToMany(Product, {through: CartItem})
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -65,5 +26,6 @@ Product.belongsTo(User, {
 module.exports = {
   db,
   User,
-  Product
+  Product,
+  Order
 }
