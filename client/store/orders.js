865cb098debbba2e51eef23d84d58b134b538d6a
tabatabb7 <yoosallyh@gmail.com>
2020-11-13 15:46:41 -0500

    WIP: created POST route for cartItem

--- client/store/orders.js
+++ client/store/orders.js
@@ -4,8 +4,9 @@ import axios from 'axios'
 const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART'
 const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
 const SET_QUANTITY = 'SET_QUANTITY'
+const GET_TOTAL = 'GET_TOTAL'
 
-// ACTION
+// ACTION CREATOR
 export const addToCart = products => ({
   type: ADD_PRODUCT_TO_CART,
   products
@@ -19,4 +20,9 @@ export const removeFromCart = products => ({
 export const setQuantity = products => ({
   type: SET_QUANTITY,
   products
+})
+
+export const getTotal = product => ({
+  type: GET_TOTAL,
+  product
 })
