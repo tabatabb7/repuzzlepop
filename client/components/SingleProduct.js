b116343b65a2592060e716c5335798cdc8568ecd
Ariel <arielespo1123@gmail.com>
2020-11-17 10:50:01 -0500

    Feat: SingleProduct rendering

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,44 +1,60 @@
 import React from 'react'
 import {connect} from 'react-redux'
-
 import {Link} from 'react-router-dom'
-
-import {ProductDetail} from './ProductDetail'
+import ProductDetail from './ProductDetail'
 import {fetchSingleProduct} from '../store/singleproduct'
 
-class SingleProduct extends React.Component {
+export class SingleProduct extends React.Component {
   componentDidMount() {
     try {
       const productId = this.props.match.params.productId
-      console.log('single product this.props.match.params.productId', productId)
+      // console.log('single product this.props.match.params.productId', productId)
       this.props.fetchSingleProduct(productId)
     } catch (error) {
       console.error(error)
     }
   }
+
   render() {
     const product = this.props.product
-    console.log('this.props ', this.props)
-    console.log('product ', product)
-    if (product === {}) {
-      return <h2>Loading...</h2>
-    } else {
-      return (
-        <div>
-          <div id="single-product">
-            <ProductDetail key={product.id} product={product} />
-          </div>
-          <form>
-            <label htmlFor="quantity">Quantity: </label>
-            <input type="number" id="quantity" name="quantity" />
-            <button type="submit">Add To Cart</button>
-          </form>
-          <Link to="/products">Return to All Products</Link>
+    // console.log('this.props ', this.props)
+    // console.log('product ', product)
+
+    // if (typeof product === undefined) {
+    //   return <h2>Loading...</h2>
+    // } else {
+    //   return <div>Rendering</div>
+    // }
+    return (
+      <div className="content-wrapper">
+        <div className="single-product">
+          <ProductDetail product={product} />
         </div>
-      )
-    }
+        <Link to="/products">Return to All Products</Link>
+      </div>
+    )
+    //
+    // if (typeof product === undefined) {
+    //   return (<h2>Loading...</h2>)
+    // } else {
+    //   return (
+    //     <div>
+    //     Found object.
+    //       <div id="single-product">
+    //         <ProductDetail key={product.id} product={product} />
+    //       </div>
+    //       <form>
+    //         <label htmlFor="quantity">Quantity: </label>
+    //         <input type="number" id="quantity" name="quantity" />
+    //         <button type="submit">Add To Cart</button>
+    //       </form>
+    //       <Link to="/products">Return to All Products</Link>
+    //     </div>
+    //   )
+    // }
   }
 }
+
 const mapState = state => {
   return {
     product: state.product
